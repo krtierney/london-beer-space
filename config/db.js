166826d9937f68581bc2e781efb351c9e8d45caf
Kaitlyn Tierney <k@krtierney.com>
2016-09-09 11:31:46 +0100

    changed db env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 dbURIs = {
   test: "mongodb://localhost/ldn-beer-api-test",
   development: "mongodb://localhost/ldn-beer-api",
-  production: process.env.MONGOLAB_URI || "mongodb://localhost/ldn-beer-api-test"
+  production: process.env.MONGODB_URI || "mongodb://localhost/ldn-beer-api-test"
 }
 
 module.exports = function(env) {
