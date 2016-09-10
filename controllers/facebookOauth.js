e3c308f66f95b963a3b9d641175082a3260a8b6e
Kaitlyn Tierney <k@krtierney.com>
2016-09-10 12:27:16 +0100

    changing facebook oauth redirect to heroku from localhost

--- controllers/facebookOauth.js
+++ controllers/facebookOauth.js
@@ -10,7 +10,7 @@ function login(req, res) {
       client_id: process.env.FACEBOOK_API_KEY,
       client_secret: process.env.FACEBOOK_SECRET_KEY,
       code: req.body.code,
-      redirect_uri: "http://localhost:3000/"
+      redirect_uri: "http://london-beer-space.herokuapp.com/"
     },
     json: true
   }).then(function(access_token) {
